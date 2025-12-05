7b0da64843618bdda7076ce69e59c8fc189b6a60
Xiaolujiayou <luyurong0@163.com>
2025-12-05 10:53:11 +0800

    HuggingFace 单个文件下载模板

--- model_dataset_download/huggingface_standalone_file_download.py
+++ model_dataset_download/huggingface_standalone_file_download.py
@@ -2,7 +2,7 @@ from huggingface_hub import hf_hub_download
 import os
 
 # 替换为您的 Hugging Face 用户访问令牌
-HF_TOKEN = "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"
+HF_TOKEN = ""
 
 # 指定模型ID和文件名
 repo_id = "CompVis/stable-diffusion-v-1-4-original"
@@ -16,7 +16,7 @@ downloaded_model_path = hf_hub_download(
     repo_id=repo_id,
     filename=filename,
     token=HF_TOKEN,
-    local_dir="D:\Desktop\llm_quick_use\model_dataset_download" # 可选：指定下载到的本地目录
+    local_dir="" # 可选：指定下载到的本地目录
 )
 
 print(f"模型已成功下载到: {os.path.abspath(downloaded_model_path)}")
